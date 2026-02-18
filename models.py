e856d0f9ce9d77948f80d54e3638afb52b14c070
halfaider <castwhendamagedealt@gmail.com>
2026-02-19 00:42:13 +0900

    - fix: pydantic-settings 업데이트로 인한 오류 수정 - fix: 설정 파일 로딩 순서 변경 - refactor: 다운로드 방송 수정

--- models.py
+++ models.py
@@ -1,54 +1,17 @@
-import os
 import logging
 from pathlib import Path
-from typing import Any, Callable, cast, Sequence
+from typing import Any, Sequence
 
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
     YamlConfigSettingsSource,
-    SettingsConfigDict
+    SettingsConfigDict,
 )
-
-from .helpers import deep_merge
 
 logger = logging.getLogger(__name__)
 
 PathType = Path | str | Sequence[Path | str]
-
-
-class MergedYamlSettingsSource(YamlConfigSettingsSource):
-    """
-    사용자 yaml 설정값을 기본값과 병합하는 클래스
-    """
-
-    def __call__(self) -> dict[str, Any]:
-        user_config = super().__call__()
-        default_config = {}
-        for field_name, field in self.settings_cls.model_fields.items():
-            if field.default_factory is not None:
-                factory = cast(Callable[[], Any], field.default_factory)
-                default_config[field_name] = factory()
-        if not user_config:
-            return default_config
-        return deep_merge(default_config, user_config)
-
-    def _read_files(self, files: PathType | None) -> dict[str, Any]:
-        if files is None:
-            return {}
-        if isinstance(files, (str, os.PathLike)):
-            files = [files]
-        vars: dict[str, Any] = {}
-        for file in files:
-            file_path = Path(file).expanduser()
-            if file_path.is_file():
-                vars.update(self._read_file(file_path))
-                logger.warning(f"'{file_path.resolve()}' 파일을 불러왔습니다.")
-                # 존재하는 첫번째 파일만 로딩
-                break
-        else:
-            logger.error(f"설정 파일을 불러올 수 없습니다: {files}")
-        return vars
 
 
 class _BaseSettings(BaseSettings):
@@ -58,10 +21,10 @@ class _BaseSettings(BaseSettings):
 
     model_config = SettingsConfigDict(
         yaml_file=(
-            Path(__file__).with_name("settings.yaml"),
-            Path.cwd() / "settings.yaml",
             Path(__file__).with_name("config.yaml"),
             Path.cwd() / "config.yaml",
+            Path(__file__).with_name("settings.yaml"),
+            Path.cwd() / "settings.yaml",
         ),
         yaml_file_encoding="utf-8",
         extra="ignore",
@@ -83,11 +46,10 @@ class _BaseSettings(BaseSettings):
         dotenv_settings: PydanticBaseSettingsSource,
         file_secret_settings: PydanticBaseSettingsSource,
     ) -> tuple[PydanticBaseSettingsSource, ...]:
-        merged_yaml_settings = MergedYamlSettingsSource(settings_cls)
         # 설정값 적용 순서
         return (
             init_settings,
-            merged_yaml_settings,
+            YamlConfigSettingsSource(settings_cls),
             env_settings,
             dotenv_settings,
             file_secret_settings,
